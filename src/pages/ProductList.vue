1b079ef500bc1fd21c95b097bbf9d9e4522751e7
MingMei Lyu <sanbika2719@gmail.com>
2020-02-11 00:37:15 +0800

    A1,A2 but pagination has error with highlight

--- src/pages/ProductList.vue
+++ src/pages/ProductList.vue
@@ -4,62 +4,25 @@
     <a-layout-content :style="{ padding: '0 50px', marginTop: '64px' }">
       <a-breadcrumb :style="{ margin: '32px 0' }">
         <a-breadcrumb-item>Home</a-breadcrumb-item>
-        <a-breadcrumb-item>List</a-breadcrumb-item>
-        <a-breadcrumb-item>App</a-breadcrumb-item>
+        <a-breadcrumb-item>Product List</a-breadcrumb-item>
       </a-breadcrumb>
       <div class="product-list">
-        <a-row :gutter="24">
-          <a-col :span="6">
+        <a-list :grid="{ gutter: 32, xs: 1, sm: 2, md: 4, lg: 4, xl: 4, xxl: 4 }" :dataSource="product_list">
+          <a-list-item slot="renderItem" slot-scope="item">
             <a-card hoverable>
               <img
                 alt="example"
                 src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
                 slot="cover"
               />
-              <a-card-meta title="$27">
-                <template slot="description">a bear</template>
+              <a-card-meta :title="'$'+item.price">
+                <template slot="description">{{item.name}}</template>
               </a-card-meta>
             </a-card>
-          </a-col>
-          <a-col :span="6">
-            <a-card hoverable>
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                slot="cover"
-              />
-              <a-card-meta title="$27">
-                <template slot="description">a bear</template>
-              </a-card-meta>
-            </a-card>
-          </a-col>
-          <a-col :span="6">
-            <a-card hoverable>
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                slot="cover"
-              />
-              <a-card-meta title="$27">
-                <template slot="description">a bear</template>
-              </a-card-meta>
-            </a-card>
-          </a-col>
-          <a-col :span="6">
-            <a-card hoverable>
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                slot="cover"
-              />
-              <a-card-meta title="$27">
-                <template slot="description">a bear</template>
-              </a-card-meta>
-            </a-card>
-          </a-col>
-        </a-row>
+          </a-list-item>
+        </a-list>
       </div>
-      <a-pagination id="page" showQuickJumper :defaultCurrent="2" :total="500" @change="onChange" />
+      <a-pagination id="page" showQuickJumper :defaultCurrent=current_page :current=current_page :total=total_pages :defaultPageSize="20" @change="getProductList" />
     </a-layout-content>
     <a-layout-footer :style="{ textAlign: 'center' }">
       Ant Design ©2018 Created by Ant UED
@@ -74,24 +37,62 @@
    text-align: center;
  }
 
- /* #search-box{
-   float: right;
- } */
+ .ant-card{
+   width: 300px;
+   height: 300px;
+   margin-bottom: 30px;
+ }
+
 </style>
 =======
 
 
 <script>
+
 import TopBar from '@/components/TopBar.vue'
+import axios from 'axios'
+
   export default {
     name:'product-list',
     components:{
       TopBar
     },
-    methods: {
-      onChange() {
+    data() {
+    return {
+      total_pages : null,
+      current_page : null,
+      product_list : null,
+    }
+  },
+    created(){
+      var test = this;
+      axios
+        .get('http://rest.apizza.net/mock/6e6f588e3cad8e88bda115251aed8406/products')
+        .then((res) =>{ test.product_list = res.data.item_list;
+                        test.current_page = res.data.current_page;
+                        test.total_pages = res.data.total_pages*10;
+                        // // eslint-disable-next-line no-console
+                        // console.log(test.total_pages)
+                        // eslint-disable-next-line no-console
+                        console.log(test.product_list)
+                        // // eslint-disable-next-line no-console
+                        // console.log(test.current_page)
+                        })
 
-      },
+    },
+    methods: {
+      getProductList: function(){
+        let current = this;
+        axios
+          .get('http://rest.apizza.net/mock/6e6f588e3cad8e88bda115251aed8406/products',{
+                page: this.current_page
+                })
+          .then((res) => {
+            current.product_list = res.data.item_list
+            // // eslint-disable-next-line no-console
+            // console.log(current.product_list)
+          })
+      }
     },
   };
 </script>
