5294f987435c946dcfe933a4a57f4a2f1058fbc1
MingMei Lyu <sanbika2719@gmail.com>
2020-03-11 01:40:43 +0800

    B2 bug fixed

--- src/Router.js
+++ src/Router.js
@@ -10,44 +10,52 @@ const router =  new Router({
       path: '/',
       name: 'Product List',
       component: () => import('@/pages/ProductList.vue'),
+      meta:{need_login:false}
     },
 
     {
       path: '/product-detail/:id',
       name: 'Product Detail',
       component: () => import('@/pages/ProductDetail.vue'),
+      meta:{need_login:false}
     },
 
     {
       path: '/login',
       name: 'Login',
       component: () => import('@/pages/Login.vue'),
+      meta:{need_login:false}
     },
     {
       path: '/register',
       name: 'Register',
       component: () => import('@/pages/Register.vue'),
+      meta:{need_login:false}
     },
     {
       path:'/shopping-cart',
       name:'Shopping Cart',
       component: () => import('@/pages/ShoppingCart.vue'),
+      meta:{need_login:true}
 
     },
     {
       path: '/purchase-tracking',
       name: 'Purchase Tracking',
       component: () => import('@/pages/PurchaseTracking.vue'),
+      meta:{need_login:true}
     },
     {
       path: '/purchase-detail/:po_no',
       name: 'Purchase Detail',
       component: () => import('@/pages/PurchaseDetail.vue'),
+      meta:{need_login:true}
     },
     {
       path: '/add-product',
       name: 'Add Product',
       component: () => import('@/pages/AddProduct.vue'),
+      meta:{need_login:true}
     },
 
 
@@ -55,11 +63,13 @@ const router =  new Router({
       path: '/po-list',
       name: 'Purchase Order List',
       component: () => import('@/pages/POList.vue'),
+      meta:{need_login:true}
     },
     {
       path: '/po-processing/:po_no',
       name: 'Purchase Order Processing',
       component: () => import('@/pages/POProcessing.vue'),
+      meta:{need_login:true}
     },
 
     // for try
@@ -67,30 +77,15 @@ const router =  new Router({
       path: '/vendor-pd/:id',
       name: 'Vendor Product Detail',
       component: () => import('@/pages/VendorPD.vue'),
+      meta:{need_login:false}
     },
   ]
 })
 
 router.beforeEach((to,from,next)=>{
-  if(to.path == '/')
+  if(to.matched.some( m => m.meta.need_login))
   {
-    next()
-  }
-  else if(to.path == '/login')
-  {
-    next()
-  }
-  else if(to.path == '/product-detail/${id}')
-  {
-    next()
-  }
-  else if(to.path == '/register')
-  {
-    next()
-  }
-  else
-  {
-    if(window.localStorage.getItem('token'))
+    if(localStorage.getItem('token'))
     {
       next()
     }
@@ -98,6 +93,10 @@ router.beforeEach((to,from,next)=>{
     {
       next('/login')
     }
+  }
+  else
+  {
+    next()
   }
 })
 
